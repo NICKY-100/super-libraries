999cae163c718352b9bf5a46c495ead638db7e3b
Nicky <geesin11@gamil.com>
2023-08-31 15:52:27 +0100

    comments to make more readable

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-
+//import SuperBox and SuperGrid from respective js files.
 import SuperBox from './SuperBox.js';
 import SuperGrid from './SuperGrid.js';
-
+// new SuperGrid with height and appending to body  
+// giving grid an alignment of top
 const grid = new SuperGrid({ height: '200px' })
 grid.appendTo(document.body)
 grid.alignment('top')
 
-
-// first box created in div red
+// redBox created in div 
 const redBox = new SuperBox('div')
 console.dir(redBox.element.style)
 
 redBox.color('red')
-// append redbox to grid element
+// append redbox to grid element and give size
 redBox.appendTo(grid.element);
 redBox.size('100px', '100px');
 
-//box created in div yellow
+//box created in div (yellow)
+// append to grid element
 const yellowBox = new SuperBox('div', 'yellow', '100px', '100px')
 yellowBox.appendTo(grid.element);
 
