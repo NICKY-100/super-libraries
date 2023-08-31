ef4bbdfddc226c0cdc2beb631e6207546bee58fc
Nicky <geesin11@gamil.com>
2023-08-31 17:43:57 +0100

    correcting typo error

--- SuperElement.js
+++ SuperElement.js
@@ -2,7 +2,7 @@
 function SuperElement(tag) {
     this.element = document.createElement(tag);
 }
-// parent function and appending superelment to 
+// parent function and appending superElement to 
 SuperElement.prototype.appendTo = function (parentElement) {
     parentElement.append(this.element);
 };
