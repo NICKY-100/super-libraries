73d649d8654991ab8a7ffeb4f496851ce1d4cd56
Nicky <geesin11@gamil.com>
2023-09-13 21:55:34 +0100

    adding comments @param to function SuperSelect

--- SuperSelect.js
+++ SuperSelect.js
@@ -1,8 +1,19 @@
 import SuperElement from "./SuperElement.js"
 import SuperOption from "./SuperOption.js";
+
+/**
+ * @param {string} select Somebody's name.
+ */
 function SuperSelect() {
     SuperElement.call(this, 'select')
-    const option = new SuperOption()
+    const option = new SuperOption('option 1', 'name'['boxColor', 'green'])
+
+    //const option2 = new SuperOption('box2', 'name'['boxColor', 'blue'])
+
+    for (let i = 0; i < SuperOption.length; i++) {
+
+    }
+    option.appendTo(grid.element)
 }
 
 export default SuperSelect;
