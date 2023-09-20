10bee08393e7ca3c7ab8801a6fea8fd8fb5ce3b5
Nicky <geesin11@gamil.com>
2023-09-20 21:27:26 +0100

    ammending @param comments for superbutton constructor

--- SuperButton.js
+++ SuperButton.js
@@ -1,8 +1,10 @@
 import SuperElement from "./SuperElement.js"
 /**
- *  @param {string} button and this,
- * @param SuperButton is called with parameters this and select
- * @param element type, and innertext
+ * * create a button of specific type and text
+ * @constructor
+ * @extends SuperElement
+ * @param {string} type - the type of button used.
+ * @param {string} text - the text used.
  */
 function SuperButton(type, text) {
     SuperElement.call(this, 'button')
@@ -10,10 +12,7 @@ function SuperButton(type, text) {
     this.element.type = type
     this.element.innerText = text
 }
-/**
- * Assign the superButton to 
- * @param {object} create object that has specific type.
- */
+
 SuperButton.prototype = Object.create(SuperElement.prototype)
 
 export default SuperButton;
