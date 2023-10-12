3f3c81b108a004d008207b79cfef0f8cc084a6fa
Nicky <geesin11@gamil.com>
2023-10-12 18:42:54 +0100

    adding main.js to export super-libraries

---
+++ library/main.js
@@ -0,0 +1,21 @@
+import SuperElement from "../SuperElement.js"
+import SuperBox from "../SuperBox.js"
+import SuperButton from "../SuperButton.js"
+import SuperEvent from "../SuperElement.js"
+import SuperForm from "../SuperForm.js"
+import SuperGrid from "../SuperGrid.js"
+import SuperOption from "../SuperOption.js"
+import SuperInput from "../SuperInput.js"
+import SuperSelect from "../SuperSelect.js"
+
+export default {
+    SuperBox,
+    SuperButton,
+    SuperElement,
+    SuperEvent,
+    SuperForm,
+    SuperGrid,
+    SuperOption,
+    SuperSelect,
+    SuperInput
+}
