5a3825a88c87e1f2a20cd6600b60273a0678dd7c
Nicky <geesin11@gamil.com>
2023-10-12 18:41:01 +0100

    initial code for  superInput

--- SuperInput.js
+++ SuperInput.js
@@ -7,14 +7,27 @@ import SuperEvent from "./SuperEvent.js"
  * @example
  * const myInput = new SuperInput("")
   */
+// this function is incomplete
+function SuperInput(type, name, attributes = [['long', 'knife ', 'box1'], ['short', 'blade', 'box2']]) {
 
-function SuperInput() {
+  SuperElement.call(this, 'input')
+  this.element.type = type
+  this.element.name = name
+  this.element.setAttribute = attributes
+  for (let i = 0; i < attributes.length; i++) {
 
+    const input = new SuperInput(attributes[i][0], attributes[i][1])
+    input.appendTo(this.element)
+    //const textInput = new SuperInput('text', "todo-item")
+    //const button = new SuperInput('button', "submit" , [
+    // [ " value"  "submit"]
+    // ])
+    // const todoForm
+  }
 }
+
 SuperInput.prototype = Object.create(SuperEvent.prototype)
 
 
 export default SuperEvent;
 
-// {/* < input type = "text" id = "name" name = "name" required minlength = "4"
-//     /> */}
