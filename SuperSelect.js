bde06fb163d21bfb041511061d1e050677bea08c
Nicky <geesin11@gamil.com>
2023-11-04 20:44:11 +0000

    adding spuerconstructors and exporting default function

--- SuperSelect.js
+++ SuperSelect.js
@@ -5,11 +5,16 @@ import SuperOption from "./SuperOption.js";
  * create a select element with a list of options, each with values and text.  
  * @constructor
  * @extends SuperElement
- * @param {Array.<string[]>} option - list of values and text for superOptions. 
+ * @param {Object} select -
+ * @param {string} select.name -
+ * @param {Array.<string[]>} select.options - list of values and text for superOptions. 
  * @example 
- * const select = new SuperSelect([['1', "one"],["2", 'two']])
+ * const select = new SuperSelect({
+ *  name: 'mySelect',
+ *  options: [['1', "one"],["2", 'two']]
+ * })
  */
-function SuperSelect(name, options = [['color', 'box1'], ['one', 'box2']]) {
+function SuperSelect({ name, options }) {
     SuperElement.call(this, 'select')
     this.element.name = name
     for (let i = 0; i < options.length; i++) {
