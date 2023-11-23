0e658f7e45a4c484d3dff07194b933d5a4474bf4
Nicky <geesin11@gamil.com>
2023-11-23 19:23:45 +0000

    adding $e as constructors and removing the new key word

--- main.js
+++ main.js
@@ -1,72 +1,70 @@
-//import SuperBox and SuperGrid from respective js files.
-import { superConstructors } from './library/main';
+// import SuperBox and SuperGrid from respective js files.
+import { SuperConstructors } from './library/main'
 import $e from './library/main.js'
-const { SuperBox, SuperButton, SuperForm, SuperGrid, SuperSelect } = superConstructors
-// new SuperGrid with height and appending to body  
+
+// new SuperGrid with height and appending to body
 // giving grid an alignment of top
-const grid = new SuperGrid({ height: '100vh' })
+const grid = $e({ height: '100vh' })// superGrid
 grid.appendTo(document.body)
 grid.alignment('bottomRight')
 
-// redBox created in div 
-const redBox = new SuperBox('div')
+// redBox created in div
+const redBox = $e({ tag: 'div' })// 'div' SuperBox
 console.dir(redBox.element.style)
 
 redBox.backgroundColor('red')
 // append redbox to grid element and give size
-redBox.appendTo(grid.element);
-redBox.size('100px', '100px');
+redBox.appendTo(grid.element)
+redBox.size('100px', '100px')
 
-//box created in div (yellow)
+// box created in div (yellow)
 // append to grid element
-const yellowBox = new SuperBox('div', 'yellow', '100px', '100px')
-yellowBox.appendTo(grid.element);
+const yellowBox = $e({ tag: 'div', backgroundColor: 'yellow', width: '100px', height: '100px' })
+yellowBox.appendTo(grid.element)
 
-//extra box for exercise(BLUE)
-const blueBox = new SuperBox('div')
+// extra box for exercise(BLUE)
+const blueBox = $e({ tag: 'div' })// superBox "div"
 console.dir(blueBox.element.style)
 
 blueBox.backgroundColor('blue')
 
-blueBox.appendTo(grid.element);
-blueBox.size('100px', '100px');
+blueBox.appendTo(grid.element)
+blueBox.size('100px', '100px')
 
-// create a new superselect 
-const select = new SuperSelect('box-alignment',
-    [
-        ["bottomRight", "bottom right"],
-        ["bottomLeft", "bottom left"],
-        ["bottomCenter", "bottom center"],
-        ["stretch", "stretch"],
-        ["left", "left"],
-        ["right", "right"],
-        ["center", "center"],
-        ["topRight", "top right"],
-        ["topLeft", "top left"],
-        ["topCenter", "top center"],
-        ["spaceBetween", "space between"],
-        ["spaceEvenly", "space evenly"],
-        ["spaceAround", "space around"]
+// create a new superselect
+const select = $e('box-alignment', // new
+  [
+    ['bottomRight', 'bottom right'],
+    ['bottomLeft', 'bottom left'],
+    ['bottomCenter', 'bottom center'],
+    ['stretch', 'stretch'],
+    ['left', 'left'],
+    ['right', 'right'],
+    ['center', 'center'],
+    ['topRight', 'top right'],
+    ['topLeft', 'top left'],
+    ['topCenter', 'top center'],
+    ['spaceBetween', 'space between'],
+    ['spaceEvenly', 'space evenly'],
+    ['spaceAround', 'space around']
 
-    ]
+  ]
 )
 
 // submit button to create new button
 // create a new superForm and appending to form
-const submit = new SuperButton('submit', 'submit')
-const form = new SuperForm()
+const submit = $e({ type: 'submit', options: 'submit' }) // SuperButton
+const form = $e()// SuperForm
 select.appendTo(form.element)
 submit.appendTo(form.element)
 form.appendTo(document.body)
 form.submit(() => {
-    const alignment = form.getVal('box-alignment')
-    grid.alignment(alignment)
+  const alignment = form.getVal('box-alignment')
+  grid.alignment(alignment)
 })
 
-const smallBox = $e('SuperBox', { tag: "div", backgroundColor: "blue", height: "100px", width: "100px" })
-//main.js
-
+const smallBox = $e('SuperBox', { tag: 'div', backgroundColor: 'blue', height: '100px', width: '100px' })
+// main.js
 
 $e('superElement', { height: '100px', width: '100px' })
-$e('superGrid', { alignment: "center" })
-
+$e('superGrid', { alignment: 'center' })
