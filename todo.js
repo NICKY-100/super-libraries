9158cdb44b003659e537381f1ae84a1a53d19239
Nicky <geesin11@gamil.com>
2023-12-10 20:34:04 +0000

    appending select, submit and todo to form

--- todo.js
+++ todo.js
@@ -8,9 +8,12 @@ const todo = $e('SuperInput', { type: 'text', name: 'todoitem' })
 
 const submit = $e('SuperButton', { type: 'submit', text: 'submit' })
 
+const select = $e('SuperSelect', { type: 'checkbox', name: 'todoitem' })
+
 todo.appendTo(form)
 
 submit.appendTo(form)
+select.appendTo(form)
 
 form.appendTo(document.body)
 form.submit(() => {
