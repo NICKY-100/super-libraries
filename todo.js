a827dc2daa3bc9df15f73a5d9f217b7ab1d6d479
Nicky <geesin11@gamil.com>
2023-12-13 22:32:58 +0000

    commented out for later useage

--- todo.js
+++ todo.js
@@ -8,15 +8,35 @@ const todo = $e('SuperInput', { type: 'text', name: 'todoitem' })
 
 const submit = $e('SuperButton', { type: 'submit', text: 'submit' })
 
-const select = $e('SuperSelect', { type: 'checkbox', name: 'todoitem' })
+const addBtn = $e('SuperButton', { type: 'submit', text: 'delete' })
+// const select1 = $e('SuperButton', { type: 'button', text: 'complete-all' })
+// const select2 = $e('SuperButton', { type: 'button', text: 'show-completed' })
+// const select3 = $e('SuperButton', { type: 'button', text: 'show-active' })
+
+const list = $e('SuperBox', { tag: 'ol' })
 
 todo.appendTo(form)
 
 submit.appendTo(form)
-select.appendTo(form)
+// select1.appendTo(form)
+// select2.appendTo(form)
+// select3.appendTo(form)
 
 form.appendTo(document.body)
+// list represents ol
+list.appendTo(document.body)
 form.submit(() => {
-  const todo = form.getVal('todoitem')
-  console.log(todo)
+  // list1 is li
+  const list1 = $e('SuperBox', { tag: 'li' })
+  list1.appendTo(list)
+
+  // const addBtn = $e('SuperButton', { type: 'submit', text: 'delete' })
+  // added last session
+  addBtn.appendTo(list)
+
+  const todoValue = form.getVal('todoitem')
+  list1.addText(todoValue)
+  console.log(todoValue)
+  todo.clear()
 })
+// line 35, 11
