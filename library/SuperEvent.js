83e91552de9bca99ef9d24d031aa244924bc7414
Nicky <geesin11@gamil.com>
2023-12-14 20:12:59 +0000

    adding a click event function to superevent

--- library/SuperEvent.js
+++ library/SuperEvent.js
@@ -1,5 +1,3 @@
-import SuperElement from './SuperElement.js'
-
 /**
  * This callback is used after an event listener.
  * @callback SuperEvent~eventCallback
@@ -16,9 +14,8 @@ import SuperElement from './SuperElement.js'
  * const form = new SuperEvent('form')
  */
 function SuperEvent (tag) {
-  SuperElement.call(this, tag)
+
 }
-SuperEvent.prototype = Object.create(SuperElement.prototype)
 
 /**
  * adding an event listener on submit to the element.
@@ -30,6 +27,20 @@ SuperEvent.prototype = Object.create(SuperElement.prototype)
  */
 SuperEvent.prototype.submit = function (callback) {
   this.element.addEventListener('submit', (e) => {
+    e.preventDefault()
+    callback(e)
+  })
+}
+/**
+ * adding an event listener on click to the element.
+ * @param {SuperEvent~eventCallback} callback -  This callback is used after an event listener.
+ * @example
+ * form.click(function(event){
+ *  console.log("hello world")
+ * })
+ */
+SuperEvent.prototype.click = function (callback) {
+  this.element.addEventListener('click', (e) => {
     e.preventDefault()
     callback(e)
   })
